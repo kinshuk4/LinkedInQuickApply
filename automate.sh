935a696875f29537b8f7a49dd050d49dc6616cb5
Kinshuk Chandra <kinshuk.ram@gmail.com>
2017-02-04 13:17:15 +0530

    now apply at the will

--- automate.sh
+++ automate.sh
@@ -2,36 +2,48 @@
 source activate per3
 alias quickApply2='python quickApply2.py --username $lusername --password $lpassword  --count $lcount --keywords $lkeyword --location $llocation --resume $lresume'
 
-# llocation="United-States"
-# llocation="Canada"
-# llocation="United-Kingdon"
-# llocation="Germany"
-# llocation="Spain"
-# llocation="Switzerland"
-# llocation="Australia"
-# llocation="New-Zealand"
-# llocation="Denmark"
-# llocation="Sweden"
 
 declare -a locArr=("United-States" "Canada" "United-Kingdon" "Germany" "Spain" "France" "Switzerland" "Netherlands" "Australia" "New-Zealand" "Denmark"  "Sweden" "Norway" "Finland")
-declare -a keyArr=("Android Developer" "Big\ Data \Developer" "Full\ Stack\ Developer" "Backend Engineer" "Java-Develper")
-declare -a resArr=("/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/java/Kinshuk_Chandra_j.pdf" 
-"/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/android/Kinshuk_Chandra_a.pdf" 
-"/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/bigdata/Kinshuk_Chandra_b.pdf" "/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/polyglot/Kinshuk_Chandra.pdf" "/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/python/Kinshuk_Chandra_p.pdf" "/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/fullstack/Kinshuk_Chandra_f.pdf")
 
-# associative array
-# declare -a keyArr
-# keyArr["Java-Developer"]="/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/java/Kinshuk_Chandra_j.pdf"
+javaRes="/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/java/Kinshuk_Chandra_j.pdf"
+androidRes="/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/android/Kinshuk_Chandra_a.pdf"
+bigdataRes="/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/bigdata/Kinshuk_Chandra_b.pdf"
+polyglotRes="/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/polyglot/Kinshuk_Chandra.pdf"
+pythonRes="/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/python/Kinshuk_Chandra_p.pdf"
+fullStackRes="/Users/kchandra/Lyf/Syncs/GoogleDrive/student/cv/fullstack/Kinshuk_Chandra_f.pdf"
 
-## now loop through the above array
+keyAD='Android-Developer'
+#keySAD='Senior-Android-Developer'
+keyBD='Bigdata-Developer' 
+keySDTL='Senior-Data-Team-Lead'
+keySpark='Apache-Spark'
+keyHibernate='hibernate'
+keyJpa='JPA'
+keySpring='Spring-Framework'
+keyJD='Java-Develper'
+keySJD='Senior-Java-Develper'
+keyJava='java'
+keyPD='Python-Developer'
+keyPolyD='Polyglot-Developer'
+keyFSD='Full-Stack-Developer'
+
+declare -a keyArr=($keyAD $keyBD $keySDTL $keySpark $keyHibernate $keyJpa $keySpring $keyJD $keySJD $keyJava $keyPD $keyPolyD $keyFSD)
+
 hash_index() {
     case $1 in
-        'Java-Developer') return 0;;
-        'Android-Developer') return 1;;
-        'Bigdata-Developer') return 2;;        
-        'Polyglot-Developer') return 3;;
-        'Python-Developer') return 4;;
-        'Full\ Stack\ Developer') return 5;;
+        $keyAD) echo $androidRes;;
+        $keySAD) echo $androidRes;;
+        $keyBD) echo $bigdataRes;;        
+        $keySpark) echo $bigdataRes;;
+        $keyHibernate) echo $javaRes;;
+        $keyJpa) echo $javaRes;;
+        $keySpring) echo $javaRes;;
+        $keyJD) echo $javaRes;;
+        $keySJD) echo $javaRes;;
+        $keyJava) echo $javaRes;;
+        $keyPD) echo $pythonRes;;
+        $keyPolyD) echo $polyglotRes;;
+        $keyFSD) echo $fullstack;;
     esac
 }
 lcount=1
@@ -43,8 +55,8 @@ do
    for key in "${keyArr[@]}";
    do    
     lkeyword="$key"
-    hash_index $lkeyword
-    lresume=${resArr[$?]}
+    lresume=$(hash_index $lkeyword)
+    echo $lkeyword $lresume
     python quickApply2.py --username $lusername --password $lpassword  --count $lcount --keywords $lkeyword --location $llocation --resume $lresume
    done
    sleep 5
